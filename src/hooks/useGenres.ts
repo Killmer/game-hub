49d94439792c707065eaf6cc26e1c39ac41e7bd4
Dima Lytvyn <killmermail@gmail.com>
2024-06-30 15:05:24 +0300

    Refactor: Create lookup hooks

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,4 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
+import genres from "../data/genres";
 import APIClient from "../services/api-client";
 
 const apiClient = new APIClient<Genre>("/genres");
@@ -13,7 +14,8 @@ const useGenres = () => {
   return useQuery({
     queryKey: ["genres"],
     queryFn: () => apiClient.getAll(),
-    staleTime: 24 * 60 * 60 * 1000, //24h
+    staleTime: 24 * 60 * 60 * 1000, //24h,
+    initialData: genres,
   });
 };
 

b2f323533deedd9043168feb0d4e0365934b0b9a
Dima Lytvyn <killmermail@gmail.com>
2024-07-02 14:36:45 +0300

    Handle error in router

--- src/services/routes.tsx
+++ src/services/routes.tsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "../pages/Layout";
 import HomePage from "../pages/HomePage";
 import GameDetailPage from "../pages/GameDetailPage";
+import ErorrPage from "../pages/ErorrPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErorrPage />,
     children: [
       {
         index: true,
